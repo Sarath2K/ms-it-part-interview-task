0cfd18f2dc3059e668f8eeb38ed57bff415c41fa
Sarath2K <sarath2kdop@gmail.com>
2023-08-18 13:18:33 +0530

    Dashboard Completed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,13 +5,119 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+    <div class="container pb-3">
+        <div class="row pt-5">
+            <div class="col-xxl-4 col-md-6">
+                <div class="card info-card sales-card">
+                    <div class="card-body">
+                        <h5 class="card-title">Employees</h5>
+                        <table class="table table-bordered table-sm">
+                            <tbody>
+                            <tr>
+                                <th>Total</th>
+                                <td>{{ $employeeCount['total'] }}</td>
+                            </tr>
+                            <tr>
+                                <th>Admin</th>
+                                <td>{{ $employeeCount[ROLE_ADMIN] }}</td>
+                            </tr>
+                            <tr>
+                                <th>Employee</th>
+                                <td>{{ $employeeCount[ROLE_EMPLOYEE] }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div><!-- End Employees Card -->
+            <div class="col-xxl-4 col-md-6">
+                <div class="card info-card sales-card">
+                    <div class="card-body">
+                        <h5 class="card-title">This Month Employees</h5>
+                        <table class="table table-bordered table-sm">
+                            <tbody>
+                            <tr>
+                                <th>Total</th>
+                                <td>{{ $employeeCount['this_month'] }}</td>
+                            </tr>
+                            <tr>
+                                <th>Active</th>
+                                <td>{{ $employeeCount['active_employee'] }}</td>
+                            </tr>
+                            <tr>
+                                <th>Inactive</th>
+                                <td>{{ $employeeCount['in_active_employee'] }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div><!-- End Employees Card -->
+            <div class="col-xxl-4 col-md-6">
+                <div class="card info-card sales-card">
+                    <div class="card-body">
+                        <h5 class="card-title">Today Employees</h5>
+                        <table class="table table-bordered table-sm">
+                            <tbody>
+                            <tr>
+                                <th>Total</th>
+                                <td>{{ $employeeCount['today'] }}</td>
+                            </tr>
+                            <tr>
+                                <th>Active</th>
+                                <td>{{ $employeeCount['today_active_employee'] }}</td>
+                            </tr>
+                            <tr>
+                                <th>Inactive</th>
+                                <td>{{ $employeeCount['today_inactive_employee'] }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div><!-- End Employees Card -->
+        </div>
+
+        <div class="pt-4">
+            <div class="card">
+                <div class="card-body p-5">
+                    <h5 class="card-title">Employees Chart</h5>
+                    <canvas id="barChart"></canvas>
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    var employeeCounts = @json($employeeCount);
+    var monthNames = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JULY', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var ctx = document.getElementById('barChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: monthNames,
+                datasets: [{
+                    label: 'Employees',
+                    data: Object.values(employeeCounts['by_month']),
+                    backgroundColor: [
+                        'rgba(54, 162, 235, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(54, 162, 235, 1)',
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    });
+</script>
--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -36,13 +36,22 @@
                                     <div>
                                         <a type="button" class="btn btn-sm btn-info"
                                            href="{{ route('employees.show',$employee->id) }}">
-                                            <i class="bi bi-eye"></i>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                 fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                                                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+                                                <path
+                                                    d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+                                            </svg>
                                         </a>
                                     </div>
                                     <div>
                                         <a type="button" class="btn btn-sm btn-warning"
-                                           href="{{ route('employees.edit',$employee->id) }}"><i
-                                                class="ri-file-edit-line"></i>
+                                           href="{{ route('employees.edit',$employee->id) }}">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                 fill="currentColor" class="bi bi-pen-fill" viewBox="0 0 16 16">
+                                                <path
+                                                    d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001z"/>
+                                            </svg>
                                         </a>
                                     </div>
                                     <div>
@@ -51,7 +60,11 @@
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" class="btn btn-sm btn-danger">
-                                                <i class="ri-delete-bin-6-line"></i>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                     fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                                                    <path
+                                                        d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
+                                                </svg>
                                             </button>
                                         </form>
                                     </div>
